68d7d2373a654319d951741966f0eacd2d2713e7
JCYepesGarcia <jcyepesgarcia@gmail.com>
2021-11-16 00:02:41 -0500

    Ajuste de comentarios

--- assets/script/validacion.js
+++ assets/script/validacion.js
@@ -3,9 +3,9 @@ document.getElementById('nickName').focus();
 const form = document.getElementById('user-form');
 const submitButton = document.getElementById('submit-btn');
 
-let timeout = null;//Se crea esta variable para agegar un tiempo de espera en el evento 'keydown' con el método setTimeout(), y que el evento no recoja de uno en uno los caracteres digitados por el ususario.
+let timeout = null;//Se crea esta variable para agregar un tiempo de espera en el evento 'keydown' con el método setTimeout(), y que el evento no recoja de uno en uno los carácteres digitados por el ususario.
 
-//El objeto errors contiene los diferentes atributos name, de cada uno de los elementos input contenidos en las etiquetas div con clase .form-box 
+//El objeto errors contiene los diferentes atributos name, de cada uno de los elementos input contenidos en las etiquetas div con clase .form-box.
 let errors = {
     nickName: true,
     email: true,
@@ -19,24 +19,24 @@ document.querySelectorAll('.form-box').forEach((form_box) => {
     let boxInput = form_box.querySelector('input');
 
     boxInput.addEventListener('keydown', () => {
+
         clearTimeout(timeout);
-        //El usuario tiene un tiempo de espera de 0.5 seg una vez se detiene de digitar para continuar digitando, antes de que el evento se reinicie.
+        
         timeout = setTimeout(() => {
-            //console.log(`Input ${boxInput.name} value: ${form_box.querySelector('input').value}`);
-            validation(form_box, boxInput);//Validación del Nombre
-        }, 500);
+            validation(form_box, boxInput);
+        }, 500);//El usuario tiene un tiempo de espera de 0.5 seg una vez se detiene al digitar, antes de que el evento se reinicie.
     });
 
 });
 
-//La función valida si el ususario ingresa un nombre. Si no ingresa un nombre agrega la clase .form-error al div form_box. Si ingresa un nombre agrega la clase .form-success al div form_box. 
+//La función valida si el ususario ingresa información en los input. Si no lo hace agrega la clase .form-error al div form_box. Si lo hace agrega la clase .form-success al div form_box. 
 validation = (form_box, boxInput) =>{
 
     if(boxInput.value==''){
         showErrorOrSuccess(true, form_box, boxInput);
     }else{
         showErrorOrSuccess(false, form_box, boxInput);
-    };
+    };//Validación genérica si el usuario deja un ítem sin diligenciar.
 
     if(boxInput.name=='email'){
         if(!boxInput.value.match(mailFormatRegex)){
@@ -44,7 +44,7 @@ validation = (form_box, boxInput) =>{
         }else{
             showErrorOrSuccess(false, form_box, boxInput);
         }
-    };
+    };//Validación de la estructura de un email.
 
     if(boxInput.name=='password'){
         if(boxInput.value.length <= 6){
@@ -52,11 +52,12 @@ validation = (form_box, boxInput) =>{
         }else{
             showErrorOrSuccess(false, form_box, boxInput);
         }
-    }
+    };//Validación de que el password ingresado contenga más de 6 carácteres.
 
     sumitController();
 }
 
+//Función de errores y validación. Agrega o elimina las clases .form-success y .form-error dependiendo del caso evaluado.
 showErrorOrSuccess = (check, form_box, boxInput) => {
     if(check){
         form_box.classList.remove('form-success');
@@ -80,17 +81,17 @@ sumitController = () =>{
 }
 
 
-//Este código es para mostrar por consola lo que esta devolviendo el formulario 
+//Este código es para mostrar por consola los datos ingresados al formulario. 
 form.addEventListener('submit', (event) => {
     
     event.preventDefault();//Evita el comportamiento por defecto del botón sumit
     
-    const formData = new FormData(event.target);//Encapsulamiento de todos los datos del formulario en una variable a través de la clase FormData
+    const formData = new FormData(event.target);//Encapsulamiento de todos los datos del formulario en una variable a través de la clase FormData.
     
-    console.log([...formData]);//Muestra los datos en consola en un array
+    console.log([...formData]);//Muestra los datos por consola en un array.
     
     for(let [key, value] of formData.entries()){
         console.log(`${key}: ${value}`);
-    }//Muestra los datos por consola de manera más amigable
+    }//Muestra los datos por consola de manera más amigable.
     
 });
